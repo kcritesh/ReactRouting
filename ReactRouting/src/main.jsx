ccd8be1b6b31724578538a5c165f47839177de44
ritesh.azminds <ritesh.azminds@gmail.com>
2023-12-21 21:13:00 +0545

    routes fix

--- ReactRouting/src/main.jsx
+++ ReactRouting/src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
-import Home from './components/Home/Home.jsx'
-import About from './components/About/About.jsx'
-import Contact from './components/Contact/Contact.jsx'
+import Home from './App.jsx'
+// import About from './components/About/About.jsx'
+import Contact from "./components/Contact/Contact.jsx"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
+      {/* <Route path='about' element={<About />} /> */}
       <Route path='contact' element={<Contact />} />
     </Route>
   )
